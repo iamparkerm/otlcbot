929bcdb3505a8330058ff039f4e55bd82a688294
iamparkerm <iamparkerm@gmail.com>
2019-04-14 13:30:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,83 @@
-import logging
-import os
-from queue import Queue
-
-import cherrypy
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
-
-
-class SimpleWebsite(object):
-    @cherrypy.expose
-    def index(self):
-        return """<H1>Welcome!</H1>"""
+"""
+the main script for running the telegram bot in python3.7
+"""
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import random
+import requests
+#import json
 
 
-class BotComm(object):
-    exposed = True
-
-    def __init__(self, TOKEN, NAME):
-        super(BotComm, self).__init__()
-        self.TOKEN = TOKEN
-        self.NAME=NAME
-        self.bot = telegram.Bot(self.TOKEN)
-        try:
-            self.bot.setWebhook("https://{}.herokuapp.com/{}".format(self.NAME, self.TOKEN))
-        except:
-            raise RuntimeError("Failed to set the webhook")
-
-        self.update_queue = Queue()
-        self.dp = Dispatcher(self.bot, self.update_queue)
-
-        self.dp.add_handler(CommandHandler("start", self._start))
-        self.dp.add_handler(MessageHandler(Filters.text, self._echo))
-        self.dp.add_error_handler(self._error)
-
-    @cherrypy.tools.json_in()
-    def POST(self, *args, **kwargs):
-        update = cherrypy.request.json
-        update = telegram.Update.de_json(update, self.bot)
-        self.dp.process_update(update)
-
-    def _error(self, error):
-        cherrypy.log("Error occurred - {}".format(error))
-
-    def _start(self, bot, update):
-        update.effective_message.reply_text("Hi!")
+TOKEN = "inserttokenhere"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+#OTLC telegram chat id
+#CHATID = '-1001320128437'
+PICS = ('https://dubsism.files.wordpress.com/2018/09/burt-reynolds-naked.jpg',
+        'https://mfas3.s3.amazonaws.com/styles/grid-3_thumbnail_retina/s3/zardozsfw_0.jpg',
+        'https://i.ebayimg.com/images/g/GzMAAOSwgkRVUSKD/s-l500.jpg')
+KEVIN = ('https://goo.gl/maps/4nwWjyZi5Y82',
+         'https://goo.gl/maps/8PAy8vgALgp',
+         'https://goo.gl/maps/RhW3StqJWx72',
+         'https://goo.gl/maps/8yiKtKEMGYR2')
+HUMAN = 'CQADAQADrAADnXJhRNhVTrHIxfGZAg'
 
 
-    def _echo(self, bot, update):
-        update.effective_message.reply_text(update.effective_message.text)
+#def get_url(url):
+#    contents = requests.get(url)
+#    url = contents['url']
+#    return url
+
+#def get_json_from_url(url):
+#    content = get_url(url)
+#    js = json.loads(content)
+#    return js
 
 
-if __name__ == "__main__":
-    # Set these variable to the appropriate values
-    TOKEN = "Your token from @Botfather"
-    NAME = otlcbot
+#def get_updates(offset=None):
+#    url = URL + "getUpdates"
+#    if offset:
+#        url += "?offset={}".format(offset)
+#    js = get_json_from_url(url)
+#    return js
 
-    # Port is given by Heroku
-    PORT = os.environ.get('PORT')
 
-    # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
+#def get_last_update_id(updates):
+#    update_ids = []
+#    for update in updates["result"]:
+#        update_ids.append(int(update["update_id"]))
+#    return max(update_ids)
 
-    # Set up the cherrypy configuration
-    cherrypy.config.update({'server.socket_host': '0.0.0.0', })
-    cherrypy.config.update({'server.socket_port': int(PORT), })
-    cherrypy.tree.mount(SimpleWebsite(), "/")
-    cherrypy.tree.mount(BotComm(TOKEN, NAME),
-                        "/{}".format(TOKEN),
-                        {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}})
-    cherrypy.engine.start()
+
+#def send_message(text, chat_id):
+#    text = urllib.parse.quote_plus(text)
+#    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+#    get_url(url)
+
+
+def cookie(bot, update):
+    url = random.choice(PICS)
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url, caption="soft as a mouse's ear")
+
+
+def areyoufriendly(bot, update):
+    chat_id = update.message.chat_id
+    bot.send_audio(chat_id=chat_id, audio=HUMAN)
+
+
+def whereiskevin(bot, update):
+    chat_id = update.message.chat_id
+    text = random.choice(KEVIN)
+    bot.sendMessage(chat_id=chat_id, text=text, caption="whew he's quick")
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('givemeacookie', cookie))
+    dp.add_handler(CommandHandler('areyoufriendly', areyoufriendly))
+    dp.add_handler(CommandHandler('whereiskevin', whereiskevin))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
